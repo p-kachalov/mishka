2a21f104953da4580b5fb97097edea5617019714
Pavel K <pa.kachalov@gmail.com>
2018-02-14 11:41:48 +0300

    gulp build

--- gulpfile.js
+++ gulpfile.js
@@ -13,6 +13,8 @@ var webp = require("gulp-webp");
 var svgstore = require("gulp-svgstore");
 var posthtml = require("gulp-posthtml");
 var include = require("posthtml-include");
+var run = require("run-sequence");
+var del = require("del");
 
 gulp.task("style", function() {
   gulp
@@ -20,11 +22,29 @@ gulp.task("style", function() {
     .pipe(plumber())
     .pipe(sass())
     .pipe(postcss([autoprefixer()]))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(minify())
     .pipe(rename("style.min.css"))
-    .pipe(gulp.dest("source/css"));
-  //.pipe(server.stream());
+    .pipe(gulp.dest("build/css"));
+});
+
+gulp.task("sprite", function() {
+  return gulp
+    .src("source/img/*_sp.svg")
+    .pipe(
+      svgstore({
+        inlineSvg: true
+      })
+    )
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("html", function() {
+  return gulp
+    .src("source/*.html")
+    .pipe(posthtml([include()]))
+    .pipe(gulp.dest("build"));
 });
 
 gulp.task("images", function() {
@@ -47,28 +67,25 @@ gulp.task("webp", function() {
     .pipe(gulp.dest("source/img"));
 });
 
-gulp.task("sprite", function() {
+gulp.task("copy", function() {
   return gulp
-    .src("source/img/*_sp.svg")
-    .pipe(
-      svgstore({
-        inlineSvg: true
-      })
-    )
-    .pipe(rename("sprite.svg"))
-    .pipe(gulp.dest("source/img"));
+    .src(["source/fonts/**/*.{woff,woff2}", "source/img/**", "source/js/**"], {
+      base: "source"
+    })
+    .pipe(gulp.dest("build"));
 });
 
-gulp.task("html", function() {
-  return gulp
-    .src("source/*.html")
-    .pipe(posthtml([include()]))
-    .pipe(gulp.dest("source"));
+gulp.task("clean", function() {
+  return del("build");
 });
 
-gulp.task("serve", ["style"], function() {
+gulp.task("build", function(done) {
+  run("clean", "copy", "style", "sprite", "html", done);
+});
+
+gulp.task("serve", function() {
   server.init({
-    server: "source/",
+    server: "build/",
     notify: false,
     open: true,
     cors: true,
@@ -76,5 +93,5 @@ gulp.task("serve", ["style"], function() {
   });
 
   gulp.watch("source/sass/**/*.{scss,sass}", ["style"]);
-  gulp.watch("source/*.html").on("change", server.reload);
+  gulp.watch("source/*.html", ["html"]);
 });
