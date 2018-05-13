807035e50eab45891d0ddd55d44ec52730102b2c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-05-13 21:59:59 +0000

    Deploy p-kachalov/235268-mishka to github.com/p-kachalov/235268-mishka.git:gh-pages

---
+++ source/js/script.js
@@ -0,0 +1,59 @@
+var navMain = document.querySelector(".main-nav");
+var navToggle = document.querySelector(".main-nav__menu-togler");
+
+navMain.classList.remove("main-nav_nojs");
+
+navToggle.addEventListener("click", function() {
+  if (navMain.classList.contains("main-nav__closed")) {
+    navMain.classList.remove("main-nav__closed");
+    navMain.classList.add("main-nav__opened");
+  } else {
+    navMain.classList.add("main-nav__closed");
+    navMain.classList.remove("main-nav__opened");
+  }
+});
+
+
+var modalWindow = document.querySelector(".modal-window");
+var orderButton = document.querySelector(".popular-product__order-button");
+var cartButtons = document.querySelectorAll(".product-card__cart-button");
+var modalAddButton = document.querySelector(".modal-window__add-button");
+
+if (orderButton) {
+  orderButton.addEventListener("click", function() {
+    if (!modalWindow.classList.contains("modal-window_shown")) {
+      modalWindow.classList.add("modal-window_shown");
+    }
+  });
+}
+
+for (var i = 0; i < cartButtons.length; i++) {
+  var item = cartButtons[i];
+  item.addEventListener("click", function() {
+    if (!modalWindow.classList.contains("modal-window_shown")) {
+      modalWindow.classList.add("modal-window_shown");
+    }
+  });
+}
+
+if (modalAddButton) {
+  modalAddButton.addEventListener("click", function() {
+    modalWindow.classList.remove("modal-window_shown");
+  });
+}
+
+window.addEventListener("keydown", function(evt) {
+  if (evt.keyCode === 27) {
+    if (modalWindow.classList.contains("modal-window_shown")) {
+      modalWindow.classList.remove("modal-window_shown");
+    }
+  }
+});
+
+window.onclick = function(event) {
+  if (event.target == modalWindow) {
+    if (modalWindow.classList.contains("modal-window_shown")) {
+      modalWindow.classList.remove("modal-window_shown");
+    }
+  }
+}
